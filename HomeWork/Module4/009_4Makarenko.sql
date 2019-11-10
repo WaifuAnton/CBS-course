78a0e64a2b76bdbc60b7c83745a188343e9079f2
WaifuAnton <WaifuAnton@users.noreply.github.com>
2019-11-10 23:31:31 +0200

    8th sql HW

---
+++ HomeWork/Module4/009_4Makarenko.sql
@@ -0,0 +1,34 @@
+USE joinsdb;
+
+SELECT * FROM employees;
+SELECT * FROM info;
+SELECT * FROM positions;
+
+DELETE FROM employees WHERE id = 6;
+
+INSERT INTO info (id_employee, birthday, status, city)
+VALUES
+(6, "1989-12-3", "married", "Poznan");
+
+INSERT INTO info (id_employee, birthday, status, city)
+VALUES
+(9, "1970-11-2", "married", "Poznan");
+
+INSERT INTO positions (id_employee, pos, salary)
+VALUES
+(6, "Office worker", 50000),
+(9, "Manager", 100000);
+
+DELETE FROM info WHERE info.id_employee = 6;
+
+DROP TRIGGER IF EXISTS delete_employee;
+
+DELIMITER |
+CREATE TRIGGER delete_employee
+BEFORE DELETE ON employees FOR EACH ROW
+BEGIN
+	DELETE FROM positions WHERE positions.id_employee = OLD.id;
+    DELETE FROM info WHERE info.id_employee = OLD.id;
+END|
+
+DELETE FROM employees WHERE id = 9;
